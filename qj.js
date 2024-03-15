11d05b616ae4eb6a381812f5b8e5466467dcec7b
Kaj Schmidt <kaj.schmidt@ntig.se>
2024-03-15 10:33:10 +0100

    koncepttest

--- qj.js
+++ qj.js
@@ -1,1 +1,35 @@
-﻿
+﻿class qj {
+    constructor(dataset) {
+        this.data = dataset;
+
+    }
+
+    query(action) {
+
+
+    }
+
+    select(action) {
+        let output = [];
+
+        if (!Array.isArray(action.table)) {
+            action.table = [action.table];
+        }
+        for (let fromTable of action.table) {
+            for (let table of this.data) {
+                if (table["name"] == fromTable) {
+                    for(let row of table["rows"]) {
+                        output.push(row);
+                        
+                    }
+                }
+            }
+        }
+
+        return output;
+    }
+
+    debug(message) {
+        console.log(message);
+    }
+}
