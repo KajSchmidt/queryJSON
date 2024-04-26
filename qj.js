d04c866490171e34f52bc0462176950cf4b126ed
Kaj Schmidt <kaj.schmidt@ntig.se>
2024-04-26 10:48:02 +0200

    sort

--- qj.js
+++ qj.js
@@ -20,7 +20,6 @@ class qj {
         "tables":[],
         "columns":[]
     }
-
     */
 
 
@@ -30,11 +29,17 @@ class qj {
         if (!Array.isArray(query["tables"])) { throw new Error("Tables must be an Array."); }
         if (query["columns"] && !Array.isArray(query["columns"])) { throw new Error("Columns must be an Array."); }
         
-        
+        let dataset = this.data;
         let output = [];
         
-        for (let table of this.data) {
+        for (let table of dataset) {
             if (query["tables"].includes(table["name"])) {
+
+                if (query["sort"]) {
+                    this.sortby(table["rows"], query["sort"])
+                }
+
+
                 for (let row of table["rows"]) {
                     if (!query["columns"]) { //Hämtar hela raden
                         output.push(row);
@@ -51,6 +56,35 @@ class qj {
         }
 
         return output;
+    }
+
+    sortby(dataset, sort, level = 0) {
+        
+        var by = sort[level];
+
+        this.debug(level);
+
+        dataset.sort((a, b) => {
+            let a_sort;
+            let b_sort;
+            if (isNaN(a[by]) || isNaN(b[by])) {
+                a_sort = a[by].toUpperCase(); 
+                b_sort = b[by].toUpperCase(); 
+            }
+            else {
+                a_sort = a[by]; 
+                b_sort = b[by];  
+            }
+            if (a_sort < b_sort)  {
+              return -1;
+            }
+            else if (a_sort > b_sort) {
+              return 1;
+            }
+            else {
+                return 0;
+            }
+          });
     }
 
     debug(message) {
