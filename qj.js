3b3bc6ef486b31f7a98a4998eb296d3ba07c83e4
Kaj Schmidt <kaj.schmidt@ntig.se>
2024-04-29 11:02:51 +0200

    Enkel filtrering

--- qj.js
+++ qj.js
@@ -1,49 +1,66 @@
-﻿/*
-
-    dataset = [
-        {
-            "name" = "",
-            "rows" = []
-        }
-    ]
-
-*/
+﻿
+/*********************************
+ * Class: qj()
+ ********************************/
 
 class qj {
     constructor(dataset) {
+        /*
+            dataset = [
+                {
+                    "name" = "",
+                    "rows" = [
+                        {}
+                    ]
+                }
+            ]
+        */
         this.data = dataset
 
     }
 
-    /*
-    query = {
-        "tables":[],
-        "columns":[]
-    }
-    */
 
+/*********************************
+ * Method: select()
+ ********************************/
 
     select(query) {
-        if (!query) { throw new Error("No query found."); }
-        if (!query["tables"]) { throw new Error("No tables found in query.") }
+        /*
+            query = {
+                "tables":[],
+                "columns":[]
+                "filters":[]
+            }
+        */
+        if (!query) { throw new Error("No query."); }
+        if (!query["tables"]) { throw new Error("No tables in query.") }
         if (!Array.isArray(query["tables"])) { query["tables"] = [query["tables"]] }
         if (query["columns"] && !Array.isArray(query["columns"])) { query["columns"] = [query["columns"]] }
-        
-        let dataset = this.data
+        if (query["filters"] && !Array.isArray(query["filters"])) { query["filters"] = [query["filters"]] }
+
+
+        let dataset = JSON.parse(JSON.stringify(this.data)) //Undviker att skicka objektreferenser
         let output = []
         
         for (let table of dataset) {
             if (query["tables"].includes(table["name"])) {
-                for (let row of table["rows"]) {
+                let rows = table["rows"];
+
+                if (query["filters"]) { //Filtrerar bort alla rows som inte passar filtren
+                    for (let filter of query["filters"]) {
+                        rows = rows.filter(filterRows => eval("filterRows." + filter))
+                    }     
+                }
+                for (let row of rows) {
                     if (!query["columns"]) { //Hämtar hela raden
-                        output.push(JSON.parse(JSON.stringify(row))) //Undviker att skicka objektreferenser istället för innehållet
+                        output.push(row) 
                     }
                     else { //Hämtar bara vissa kolumner
                         let partialRow = {};
                         for (let column of query["columns"]) {
                             if (row[column]) { partialRow[column] = row[column] } //Kollar att kolumnen faktiskt existerar
                         }
-                        output.push(JSON.parse(JSON.stringify(partialRow)))
+                        output.push(partialRow)
                     }
                 }
             }
