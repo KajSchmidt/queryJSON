91afca77f2b268ffe0f08718dee2db60ae3c3085
Kaj Schmidt <kaj.schmidt@ntig.se>
2024-04-29 09:31:03 +0200

    renare kod

--- qj.js
+++ qj.js
@@ -11,7 +11,7 @@
 
 class qj {
     constructor(dataset) {
-        this.data = dataset;
+        this.data = dataset
 
     }
 
@@ -25,12 +25,12 @@ class qj {
 
     select(query) {
         if (!query) { throw new Error("No query found."); }
-        if (!query["tables"]) { throw new Error("No tables found in query."); }
-        if (!Array.isArray(query["tables"])) { throw new Error("Tables must be an Array."); }
-        if (query["columns"] && !Array.isArray(query["columns"])) { throw new Error("Columns must be an Array."); }
+        if (!query["tables"]) { throw new Error("No tables found in query.") }
+        if (!Array.isArray(query["tables"])) {  }
+        if (query["columns"] && !Array.isArray(query["columns"])) { throw new Error("Columns must be an Array.") }
         
-        let dataset = this.data;
-        let output = [];
+        let dataset = this.data
+        let output = []
         
         for (let table of dataset) {
             if (query["tables"].includes(table["name"])) {
@@ -42,52 +42,52 @@ class qj {
 
                 for (let row of table["rows"]) {
                     if (!query["columns"]) { //Hämtar hela raden
-                        output.push(row);
+                        output.push(row)
                     }
                     else { //Hämtar bara vissa kolumner
                         let partialRow = {};
                         for (let column of query["columns"]) {
-                            if (row[column]) { partialRow[column] = row[column]; } //Kollar att kolumnen faktiskt existerar
+                            if (row[column]) { partialRow[column] = row[column] } //Kollar att kolumnen faktiskt existerar
                         }
-                        output.push(partialRow);
+                        output.push(partialRow)
                     }
                 }
             }
         }
 
-        return output;
+        return output
     }
 
     sortby(dataset, sort, level = 0) {
         
-        var by = sort[level];
+        var by = sort[level]
 
-        this.debug(level);
+        this.debug(level)
 
         dataset.sort((a, b) => {
-            let a_sort;
-            let b_sort;
+            let a_sort
+            let b_sort
             if (isNaN(a[by]) || isNaN(b[by])) {
-                a_sort = a[by].toUpperCase(); 
-                b_sort = b[by].toUpperCase(); 
+                a_sort = a[by].toUpperCase() 
+                b_sort = b[by].toUpperCase() 
             }
             else {
-                a_sort = a[by]; 
-                b_sort = b[by];  
+                a_sort = a[by]
+                b_sort = b[by]
             }
             if (a_sort < b_sort)  {
               return -1;
             }
             else if (a_sort > b_sort) {
-              return 1;
+              return 1
             }
             else {
-                return 0;
+                return 0
             }
           });
     }
 
     debug(message) {
-        console.log(message);
+        console.log(message)
     }
 }
